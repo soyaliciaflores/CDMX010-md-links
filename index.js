e8e8f01533e36e2378217ee78ec486ef180767ed
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-04-06 13:43:27 -0500

    this version gets links from files

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ const readLinks = ()=>{
     const URLs = data.match(regexURL);
     console.log(URLs);
     })};
-   
+//readLinks()
+
+const readAllLinks = () => {
+  fetch('https://github.com/')
+  .then(res => {
+      console.log(res.status);
+  })
+  .catch(err => {
+    console.log('error')
+  });
+}
+readAllLinks()
+
+const pathParse = path.parse(__filename);
+const directory = pathParse.dir
+console.log(directory)
 
 const mdPaths = (document) =>{
 fs.readdir(document, (err, files) => {
@@ -27,41 +42,41 @@ fs.readdir(document, (err, files) => {
     files.forEach(file => {
       if (path.extname(file) != ".md")
        return 
-      const onlyMds = file
-      // const dirJoined = path.join(directory, document, onlyMds)
-      // const mdAbsolutePath = dirJoined
+      let onlyMds = file
       console.log(onlyMds)
-      fs.readFile(document, 'utf8', function(err, data){
-       console.log(data)
+      onlyMds = path.join(directory, document, onlyMds)
+      console.log(onlyMds)
+      fs.readFile(onlyMds, 'utf8', function(err, data){
+        const regexURL = /\(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}\gi/gm
+        const URLs = data.match(regexURL);
+        console.log(URLs);
       })
     })
   }})
 }
 //mdPaths(document)
 
+
+
+//Esta funcion es recursiva para saber si es carpeta o archivo 
+
+
 const whatItIs = (document) => {
-  // convertir a ruta absoluta el path enviado por el usuario
-  const absPath = path.isAbsolute(document) ? document : path.resolve(document)
-  const mdFiles = []
-  if (fs.statSync(absPath).isDirectory()) {
-    const filesAndDirs = fs.readdirSync(absPath)
-    return filesAndDirs.map(fileAndDir => whatItIs(fileAndDir))
-  } else {
-    if (absPath.endsWith('.md')) {
-      mdFiles.push(fileAndDir)
-      return mdFiles
-    } else {
-      return []
-    }
+  //convertir a ruta absoluta el path enviado por el usuario
+  const regExpFiles = /^(.+)\/([^\/]+)$/m;
+  const itsAMDFile = path.extname(document)
+  if (document.match(regExpFiles)){
+    console.log('Es una carpeta')
+    mdPaths(document);
+  
   }
-  // const regExpFiles = /^(.+)\/([^\/]+)$/m;
-  // const itsAMDFile = path.extname(document)
-  // if (document.match(regExpFiles)){
-  //   mdPaths(document);
-  // }
-  // else if (itsAMDFile === '.md'){
-  //   readLinks();
-  // }
+  else if (itsAMDFile === '.md'){
+    console.log('es un archivo md')
+    readLinks();
+  }
+  else{
+    console.log('No es carpeta ni archiv md')
+  }
 }
 whatItIs(document)
 
