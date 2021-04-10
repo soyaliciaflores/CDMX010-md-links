1d42ba4ab30de385cc9f524b968039d45dd05fbc
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-04-10 01:28:11 -0500

    it gets the links status of a file

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path');
 const fetch = require('node-fetch');
 
 //Path con el que se esta trabajando
-const document = './Files'
+//const document = './Files'
 //const document = './Files/README.md'
-//const document = 'prueba2.md'
+const document = 'prueba2.md'
 
 
 //Función que imprime el path del documento, texto y link de un solo archivo
@@ -16,17 +16,10 @@ fs.readFile(document, 'utf8', function(err, data){
         console.log(err);
     else{
     const regexURL = /\[([^[]+)\](\(.*\))/gm;
-    const regexURL2 = /\[([^[]+)\]\((.*)\)/;
     const URLs = data.match(regexURL)
-   for(let x in URLs){
-        const currentPath = (__filename)
-        console.log('----------------------------------------------'.rainbow)
-       let groupData = regexURL2.exec(URLs[x]);
-    //    console.log(groupData)
-    console.log('path: '.bgBlue + currentPath.brightBlue) 
-    console.log('link: '.bgYellow + groupData[2].slice(0,30).brightYellow)
-    console.log('Text: '.bgGreen + groupData[1].brightGreen)
-   }
+    //console.log(URLs)
+   //forInLinks(URLs)
+   //validateLinks(URLs)
 }}); 
 }
 //mdLinksFile();
@@ -46,24 +39,26 @@ const mdLinksFiles = (filePath) =>{
           onlyMds = path.join(directory, filePath, onlyMds)
           fs.readFile(onlyMds, 'utf8', function(err, data){
             const regexURL = /\[([^[]+)\](\(.*\))/gm;
-            const regexURL2 = /\[([^[]+)\]\((.*)\)/;
             const URLs = data.match(regexURL);
             //const linkStaText = URLs.forEach(link => mdLinksFile(link))
-            //console.log(linkStaText + 'console linkStaText 54')
-            for(let x in URLs){
-                const currentPath = (__filename)
-                console.log('----------------------------------------------'.rainbow)
-               let groupData = regexURL2.exec(URLs[x]);
-            //    console.log(groupData)
-            console.log('path: '.bgBlue + currentPath.brightBlue) 
-            console.log('link: '.bgYellow + groupData[2].slice(0,30).brightYellow)
-            console.log('Text: '.bgGreen + groupData[1].brightGreen)
-           }
+            forInLinks(URLs)
           })
         })
       }})
     }
     //mdLinksFiles(document)
+
+const forInLinks = (links) => {
+  const regexURL2 = /\[([^[]+)\]\((.*)\)/;
+    for(let x in links){
+      const currentPath = (__filename)
+      console.log('----------------------------------------------'.rainbow)
+     let groupData = regexURL2.exec(links[x]);
+  //    console.log(groupData)
+  console.log('path: '.bgBlue + currentPath.brightBlue) 
+  console.log('link: '.bgYellow + groupData[2].slice(0,30).brightYellow)
+  console.log('Text: '.bgGreen + groupData[1].brightGreen)
+ }}
 
 //Esta funcion es para saber si es carpeta o archivo 
 const whatItIs = (filePath) => {
@@ -84,4 +79,32 @@ const whatItIs = (filePath) => {
     }
   }
   whatItIs(document)
-  
+
+
+const validateLinks = (files)=> {
+fs.readFile(files, 'utf8', function(err, data){
+const regexURL = /\https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}\gi/gm;
+const URLs = data.match(regexURL);
+URLs.forEach(getValidateLinks)
+})};
+validateLinks(document)
+
+const getValidateLinks = (links) => {
+fetch(links)
+      .then(res => {
+          currentPath = (__filename)
+          const resStatus = (res.statusText);
+          console.log('Url:'.bgYellow + links.slice(0,200).brightYellow)
+          if(resStatus === 'OK'){
+            console.log('Status:'.bgGreen + 'OK'.brightGreen)
+          }
+          else{
+            console.log('Status:'.bgRed + 'FAIL'.brightRed)
+            
+          }
+          console.log('Path:'.bgBlue + currentPath.brightBlue)
+          console.log('----------------------------------------'.rainbow)
+      })
+      .catch(err => {
+        console.log(err)
+      })};
