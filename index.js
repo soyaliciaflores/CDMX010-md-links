a0d20ae67fa645d3ee76b4302ccd6c414d87856f
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-04-12 17:17:50 -0500

    short version validate with recursivity

--- index.js
+++ index.js
@@ -3,108 +3,96 @@ const colors = require('colors');
 const path = require('path');
 const fetch = require('node-fetch');
 
-//const document = './Files'
+const document = './Files'
 //const document = './Files/README.md'
-const document = 'prueba2.md'
+//const document = 'prueba2.md'
 
-//Función que imprime la funcion base
-const forInLinks = (links) => {
-  const regexURL2 = /\[([^[]+)\]\((.*)\)/;
-    for(let x in links){  
+// Esta función excluye todos los archivos md, reconoce los links 
+// y los regresa en forma de array
+const findMarkdownFiles = (document) => {
+  // convertir a ruta absoluta el path enviado por el usuario
+  const absPath = path.isAbsolute(document) ? document : path.resolve(document)
+  const itsAMDFile = path.extname(document)
+  if (fs.statSync(absPath).isDirectory()) {
+    const filesAndDirs = fs.readdirSync(absPath)
+    let mdFiles = []
+    filesAndDirs.forEach((fileAndDir) => {
+      const newMdFiles = findMarkdownFiles(path.join(absPath, fileAndDir))
+      mdFiles = mdFiles.concat(newMdFiles) 
+    })
+  } else if (itsAMDFile === '.md') {
+      fs.readFile(document, 'utf8', function(err, data){
+        const regexURL = /https?:\/\/[a-zA-Z\.\/-]+/gm;
+        const URLs = data.match(regexURL)
+        //funcion para mdLinks
+        //validateInLinks(URLs)
+        //funcion para flag validate
+        //URLs.forEach(getValidate)
+        //Funcion para flag stats
+        URLs.forEach(getStats)
+      });    
+  }}
+findMarkdownFiles(document);
+
+const validateInLinks = (links) => {
+    for(let link in links){
       const currentPath = (__filename)
       console.log('----------------------------------------------'.rainbow)
-     let groupData = regexURL2.exec(links[x]);
-    //console.log(groupData)
-    console.log('path: '.bgBlue + currentPath.brightBlue) 
-    console.log('link: '.bgYellow + groupData[2].slice(0,30).brightYellow)
-    console.log('Text: '.bgGreen + groupData[1].brightGreen)
+      console.log('link: '.bgYellow + links[link].slice(0,25).brightYellow + '  ---  '.rainbow + 'path: '.bgBlue + currentPath.brightBlue) 
  }}
 
-const mdLinksFile = () =>{
-  fs.readFile(document, 'utf8', function(err, data){
-      if (err)
-          console.log(err);
-      else{
-    const regexURL = /\[([^[]+)\](\(.*\))/gm;
-    const URLs = data.match(regexURL)
-    //console.log(URLs)
-    //Funcion para que muestre el resultado base de un solo archivo 
-    //forInLinks(URLs)
-    //Funcion para que muestra el resultado con la option validate
-    validateOneFile(URLs)
-     //Funcion para que muestra el resultado con la option stats
-     //statsOneFile(URLs)
-  }}); 
-  }
-  mdLinksFile(document);
-
-////////////////Funciones para flag validate/////////////////
-//Funcion para el flag validate para un solo archivo
-const validateOneFile = (links) => {
-  const regexURL2 = /\[([^[]+)\]\((.*)\)/;
-  //console.log(links)
-  const arrLinks = []
-    for(let x in links){
-      let groupData = regexURL2.exec(links[x]);
-      // console.log('---------------'.rainbow)
-      //console.log('link: '.bgYellow + groupData[2].slice(0,30).brightYellow)
-      arrLinks.push(groupData[2])  
-      //Función validate
-      //arrLinks.forEach(getValidateLinks)
-      //Funcion Stats
-      arrLinks.forEach(statsOneFile)
- }
-}
-  
-const getValidateLinks = (links) => {
+ const getValidate = (links) => {
   fetch(links)
     .then(res => {
-      currentPath = (__filename)
-      const resStatus = (res.statusText)
-      const statusCode = (res.status)
-      // --------imprime la funcion de validate ------
-      console.log('Url:'.bgYellow + links.slice(0,200).brightYellow);
-      console.log('Status: '.bgCyan + statusCode);
-      if(resStatus === 'OK'){
-        console.log('Status:'.bgGreen + 'OK'.brightGreen)
-      }
-      else{
-        console.log('Status:'.bgRed + 'FAIL'.brightRed) 
-      }
-      console.log('Path:'.bgBlue + currentPath.brightBlue)
-      console.log('----------------------------------------'.rainbow)
+        const currentData = res
+        currentPath = (__filename)
+        const resStatus = (res.statusText)
+        const statusCode = (res.status)
+        //--------imprime la funcion de validate ------
+        console.log('Url:'.bgYellow + links.slice(0,200).brightYellow);
+        console.log('Status: '.bgCyan + statusCode);
+        if(resStatus === 'OK'){
+          console.log('Status:'.bgGreen + 'OK'.brightGreen)
+        }
+        else{
+          console.log('Status:'.bgRed + 'FAIL'.brightRed) 
+        }
+        console.log('Path:'.bgBlue + currentPath.brightBlue)
+        console.log('----------------------------------------'.rainbow)
     })
     .catch(err => {
       console.log(err)
-    })
-};
+        })};
 
-///////////////////Funciones para el flag stats
-const statsOneFile = (links) => {
+const getStats = (links) => {
   fetch(links)
     .then(res => {
-      currentPath = (__filename)
       const resStatus = (res.statusText)
-      const statusCode = (res.status)
-      //console.log(links)
-
-      // // --------imprime la funcion de validate ------
-      // console.log('Url:'.bgYellow + links.slice(0,200).brightYellow);
-      // console.log('Status: '.bgCyan + statusCode);
-      // if(resStatus === 'OK'){
-      //   console.log('Status:'.bgGreen + 'OK'.brightGreen)
-      // }
-      // else{
-      //   console.log('Status:'.bgRed + 'FAIL'.brightRed) 
-      // }
-      // console.log('Path:'.bgBlue + currentPath.brightBlue)
-      // console.log('----------------------------------------'.rainbow)
-      //-----imprimir unique------
+      let objLinks = {
+        url : links,
+        status: resStatus
+      }
+        console.log(objLinks)
+      //const uniqueArr = new Set(links)
+      //console.log(uniqueArr)
+        // const currentData = res
+        // currentPath = (__filename)
+        // const resStatus = (res.statusText)
+        // console.log(resStatus)
+        // const statusCode = (res.status)
+        //--------imprime la funcion de validate ------
+        // console.log('Url:'.bgYellow + links.slice(0,200).brightYellow);
+        // console.log('Status: '.bgCyan + statusCode);
+        // if(resStatus === 'OK'){
+        //   console.log('Status:'.bgGreen + 'OK'.brightGreen)
+        // }
+        // else{
+        //   console.log('Status:'.bgRed + 'FAIL'.brightRed) 
+        // }
+        // console.log('Path:'.bgBlue + currentPath.brightBlue)
+        // console.log('----------------------------------------'.rainbow)
     })
-    const arrUnique = [...new Set(links)]
-      console.log(arrUnique)
     .catch(err => {
       console.log(err)
-    })
-    
-};
+        })};
+
